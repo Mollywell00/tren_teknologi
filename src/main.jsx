727f391a6576a90fa68e696391756a0741d59179
Mollywell00 <110741718+Mollywell00@users.noreply.github.com>
2025-07-29 17:26:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tren_teknologi">
       <Page />
     </BrowserRouter>
   </StrictMode>
