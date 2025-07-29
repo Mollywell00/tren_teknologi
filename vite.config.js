12ed93e9497e700a5186dfdf47b9e8b44a2e86aa
Mollywell00 <110741718+Mollywell00@users.noreply.github.com>
2025-07-29 17:25:35 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  base: '/tren_teknologi/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
+});
+
